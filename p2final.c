77e770f3c625b1445a409ca16921ecb823c70322
Bodduharshitha <93128787+Bodduharshitha@users.noreply.github.com>
2021-11-26 16:15:26 +0530

    Update p2final.c

--- p2final.c
+++ p2final.c
@@ -1,15 +1,14 @@
+#include <stdio.h>
+int add(int num1, int num2)
+{
+	return(num1 + num2);
+}
 int main()
 {
 	int num1, num2, result;
 	printf("Enter the two number: ");
 	scanf("%d %d", &num1, &num2);
 	result = add(num1, num2);
-	printf("\nAddition of %d and %d is %d", num1, num2, result);
+	printf("Addition of %d and %d is %d", num1, num2, result);
 	return 0;
-}
-int add(int num1, int num2)
-{
-	int result;
-	result = num1 + num2;
-	return result;
 }
