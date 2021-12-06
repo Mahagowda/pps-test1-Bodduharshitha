c6a81132dd7a45776992912cbed49d6070539aae
Bodduharshitha <93128787+Bodduharshitha@users.noreply.github.com>
2021-12-06 12:20:47 +0530

    Update p5final.c

--- p5final.c
+++ p5final.c
@@ -30,3 +30,41 @@ int main()
   output(l);
   return 0;
 }
+
+
+
+call by reference:
+
+
+#include <stdio.h>
+int cmp(int*a, int*b, int*c)
+{
+  int largest=0;
+  if(a>b && b>c)
+        largest=*a;
+  else if(b>a && b>c)
+        largest=*b;
+  else if(c>a && c>b)
+        largest=*c;
+}
+int input()
+{
+  int a;
+  printf("Enter the number:");
+  scanf("%d", &a);
+  return a;
+}
+void output(int l)
+{
+  printf("Largest number is : %d\n", l);
+}
+int main()
+{
+  int x,y,z,l;
+  x= input();
+  y= input();
+  z= input();
+  l= cmp(&x,&y,&z);
+  output(l);
+  return 0;
+}
