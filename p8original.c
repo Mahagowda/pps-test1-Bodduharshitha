63ad18eac4d3418a84fe9a435db9acfab79b67be
Bodduharshitha <93128787+Bodduharshitha@users.noreply.github.com>
2021-12-13 11:59:53 +0530

    Update p8original.c

---
+++ p8original.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float n , squareroot;
+  printf("Enter the number:");
+  scanf("%f",&n);
+  squareroot = sqrt(n);
+  printf("square root of %f is %f",n,squareroot);
+  return 0;
+}
