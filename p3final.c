135cd4ba4df7e133b5fdba985cafb0e921310884
Bodduharshitha <93128787+Bodduharshitha@users.noreply.github.com>
2021-12-27 11:40:50 +0530

    Update p3final.c

--- p3final.c
+++ p3final.c
@@ -20,3 +20,28 @@ int main()
   output(a,b,sum);
   return 0;
 }
+
+
+
+#include<stdio.h>
+int input(int a,int b)
+{
+  printf("Enter two number");
+  scanf("%d %d",&a,&b);
+}
+int add(int a,int b,int sum)
+{
+  sum = a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d + %d is %d\n",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(a,b);
+  sum = add(a,b,sum);
+  output(a,b,sum);
+  return 0;
+}
